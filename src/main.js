614433ccb3cff2fff63f741b3d6c73b86b198765
thomascarre <carrethomas7@gmail.com>
2024-10-09 19:25:02 +0200

    first commit ?

--- src/main.js
+++ src/main.js
@@ -1,3 +1,33 @@
 import './styles/style.css'
 
-console.log('Hello')
+console.log('Hello KEZ')
+
+const grid = document.querySelector('.grid__project');
+const wrapper = document.querySelector('.page-wrapper');
+
+let maxMoveX = (grid.offsetWidth - wrapper.offsetWidth) / 2;
+let maxMoveY = (grid.offsetHeight - wrapper.offsetHeight) / 2;
+
+let currentX = 0;
+let currentY = 0;
+let targetX = 0;
+let targetY = 0;
+
+wrapper.addEventListener('mousemove', (e) => {
+  let mousePercentX = e.clientX / wrapper.offsetWidth;
+  let mousePercentY = e.clientY / wrapper.offsetHeight;
+  
+  targetX = (mousePercentX - 0.5) * maxMoveX * 2.2;
+  targetY = (mousePercentY - 0.5) * maxMoveY * 2.2;
+});
+
+function animate() {
+  currentX += (targetX - currentX) * 0.09;
+  currentY += (targetY - currentY) * 0.09;
+  
+  grid.style.transform = `translate(-50%, -50%) translate(${-currentX}px, ${-currentY}px)`;
+  
+  requestAnimationFrame(animate);
+}
+
+animate();
