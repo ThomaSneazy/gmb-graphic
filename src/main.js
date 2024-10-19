ae6e8b560fbbe42cfc5d197626f17c9fb21d8773
thomascarre <carrethomas7@gmail.com>
2024-10-19 21:42:40 +0200

    try few grids

--- src/main.js
+++ src/main.js
@@ -15,8 +15,11 @@ let currentY = 0;
 let targetX = 0;
 let targetY = 0;
 
+let isAnimationEnabled = false;
 
 wrapper.addEventListener('mousemove', (e) => {
+  if (!isAnimationEnabled) return;
+
   let mousePercentX = e.clientX / wrapper.offsetWidth;
   let mousePercentY = e.clientY / wrapper.offsetHeight;
   
@@ -25,12 +28,42 @@ wrapper.addEventListener('mousemove', (e) => {
 });
 
 function animate() {
-  currentX += (targetX - currentX) * 0.09;
-  currentY += (targetY - currentY) * 0.09;
-  
-  grid.style.transform = `translate(-50%, -50%) translate(${-currentX}px, ${-currentY}px)`;
+  if (isAnimationEnabled) {
+    currentX += (targetX - currentX) * 0.09;
+    currentY += (targetY - currentY) * 0.09;
+    
+    grid.style.transform = `translate(-50%, -50%) translate(${-currentX}px, ${-currentY}px)`;
+  }
   
   requestAnimationFrame(animate);
 }
 
 animate();
+
+function adjustGridItemSizes() {
+  const gridItems = document.querySelectorAll('.grid__item');
+  
+  gridItems.forEach((item, index) => {
+    const aspect = item.getAttribute('data-aspect');
+    let gridArea = item.style.gridArea.split(' / ');
+    
+    if (aspect === 'portrait') {
+      gridArea[2] = 'span 5';
+      gridArea[3] = 'span 3';
+    } else if (aspect === 'landscape') {
+      gridArea[2] = 'span 4';
+      gridArea[3] = 'span 5';
+    }
+    
+    item.style.gridArea = gridArea.join(' / ');
+  });
+}
+
+// Appelez cette fonction après que le DOM soit chargé
+document.addEventListener('DOMContentLoaded', () => {
+  adjustGridItemSizes();
+  
+  setTimeout(() => {
+    isAnimationEnabled = true;
+  }, 5000);
+});
